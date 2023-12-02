b6a2049b33930c1d1da09970246105a57e8f09b8
Noxware <7684329+noxware@users.noreply.github.com>
2023-12-02 00:51:20 -0300

    Simplier solution for day 1 part 2

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ This repo contains my solutions for the [Advent of Code 2023](https://adventofco
 
 - `d1.txt` is the input for day 1.
 - `d1p1.rs` is the binary for day 1 part 1.
+- `d1p2v2.rs` is an alternative (version 2) solution for day 1 part 2.
 
 ## Running a solution
 
---
+++ src/bin/d1p2v2.rs
@@ -0,0 +1,30 @@
+// Simplier than my original solution and preserves everything from part 1,
+// but may be less performant.
+
+fn main() {
+    let result: u32 = std::fs::read_to_string("inputs/d1.txt")
+        .expect("Unable to read input")
+        .lines()
+        .map(|l| {
+            l.replace("one", "one1one")
+                .replace("two", "two2two")
+                .replace("three", "three3three")
+                .replace("four", "four4four")
+                .replace("five", "five5five")
+                .replace("six", "six6six")
+                .replace("seven", "seven7seven")
+                .replace("eight", "eight8eight")
+                .replace("nine", "nine9nine")
+        })
+        .map(|l| {
+            let mut iter = l.chars().filter_map(|c| c.to_digit(10));
+
+            let first = iter.next().unwrap();
+            let last = iter.last().unwrap_or(first);
+
+            first * 10 + last
+        })
+        .sum();
+
+    println!("{}", result);
+}
