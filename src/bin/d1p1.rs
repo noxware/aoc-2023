b73927e9139db62b06744d0ba20c90d7da5faaad
Noxware <7684329+noxware@users.noreply.github.com>
2023-12-01 23:13:22 -0300

    day 1 part 1

---
+++ src/bin/d1p1.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let result: u32 = std::fs::read_to_string("inputs/d1.txt")
+        .expect("Unable to read input")
+        .lines()
+        .map(|l| {
+            let mut iter = l
+                .chars()
+                .map(|c| c.to_digit(10))
+                .filter(|d| d.is_some())
+                .map(|d| d.unwrap());
+
+            let first = iter.next().unwrap();
+            let last = iter.last().unwrap_or(first);
+
+            return first * 10 + last;
+        })
+        .sum();
+
+    println!("{}", result);
+}
